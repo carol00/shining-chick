e9b47af6cb10ca1bb57ac84ca0f9da8e3e7ea837
Carol <carol1330s@gmail.com>
2017-07-15 14:09:38 +0800

    Chick

--- src/home.jsx
+++ src/home.jsx
@@ -2,4 +2,26 @@ import {h} from './link'
 
 export default props =>
 <div class="home">
+  <div class="chick">
+    <div class="body">
+      <div class="eggshell">
+        <div>
+          <div></div><div></div><div></div><div></div><div></div>
+        </div>
+      </div>
+      <div class="eyes">
+        <div></div><div></div>
+      </div>
+      <div class="mouth">
+        <div></div><div></div>
+      </div>
+      <div class="hand">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    <div class="foot">
+      <div></div><div></div>
+    </div>
+  </div>
 </div>
