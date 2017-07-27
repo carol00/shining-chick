f4010b50e0e715a6e5023677b256288b9971e338
Carol <carol1330s@gmail.com>
2017-07-27 12:15:07 +0800

    Add the component of the bowl and modify the component of the plate

--- src/feed.jsx
+++ src/feed.jsx
@@ -1,16 +1,28 @@
 import {h} from './link'
 
-const Rice = () =>
+const Plate = () =>
 <div class="plate">
   <div class="inside">
-    {Array.from({length: 65}).map(() =>
-      <div class="rice"></div>
-    )}
   </div>
 </div>
 
+const Bowl = () =>
+<div class="bowl">
+  <div class="inside">
+    <Rice />
+  </div>
+  <div class="bottom"></div>
+</div>
+
+const Rice = () =>
+<div class="rice">
+  {Array.from({length: 13}).map(() =>
+    <span></span>
+  )}
+</div>
 
 export default () =>
 <div class="feed">
-  <Rice />
+  <Bowl />
+  <Plate />
 </div>
