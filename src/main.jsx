cd8e9e9c27fa93cae4ad6a8d5b5134e30c294479
Carol <carol1330s@gmail.com>
2017-07-31 22:14:37 +0800

    Add the navigate component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import {h} from './link'
 import Home from './home'
 import {Identity} from './identity'
 import Feed from './feed'
+import Nav from './nav'
 
 export default props =>
 <div class="wrap">
   <Home />
   <Identity />
   <Feed />
+  <Nav />
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 </div>
---
+++ src/nav.jsx
@@ -0,0 +1,8 @@
+import {h} from './link'
+
+export default () =>
+<nav>
+  <div><span class="fa fa-home"></span><p>Home</p></div>
+  <div><span class="fa fa-id-card"></span><p>Profile</p></div>
+  <div><span class="fa fa-cutlery"></span><p>Feed</p></div>
+</nav>
