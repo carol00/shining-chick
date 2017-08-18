f1793546f6a6f39918a4e9313a22f7e656846512
Carol <carol1330s@gmail.com>
2017-08-18 15:51:23 +0800

    Add the header component

---
+++ src/header.jsx
@@ -0,0 +1,6 @@
+import {h} from './link'
+
+export default () =>
+<header>
+  <p class="logo">LOGO</p><h3>Shining Chick</h3>
+</header>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import {h} from './link'
 import {Nav} from './nav'
+import Header from './header'
 import {Content} from './location'
 
 export default props =>
 <div class="wrap">
+  <Header />
   <Content />
   <Nav />
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
