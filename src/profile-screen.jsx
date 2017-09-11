1e27c3aa30ea7f37964ce8fe3db7c2abbbe40ee4
Carol <carol1330s@gmail.com>
2017-09-11 13:35:09 +0800

    Add the profile layout and background

--- src/profile-screen.jsx
+++ src/profile-screen.jsx
@@ -1,14 +1,20 @@
 import {h} from './link'
 
 export default ({name, birthday, satisfaction}) =>
-<div class="identity">
-  <div class="item">
-    <label>Name：</label><span>{birthday}</span>
+<div class="profile">
+  <div class="items">
+    <div>
+      <label>Name：</label><span></span>
+    </div>
+    <div>
+      <label>Birthday：</label><span></span>
+    </div>
+    <div>
+      <label>Satisfaction：</label><span>{satisfaction}</span>
+    </div>
   </div>
-  <div class="item">
-    <label>Birthday：</label><span>{birthday}</span>
-  </div>
-  <div class="item">
-    <label>Satisfaction：</label><span>{satisfaction}</span>
+
+  <div>
+    <img src="images/chick-bg.png" />
   </div>
 </div>
