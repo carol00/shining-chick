ecdf7e2579441976e502a3422ee5f0035ff755f1
Carol <carol1330s@gmail.com>
2017-09-27 14:28:41 +0800

    Update logo

--- src/header.jsx
+++ src/header.jsx
@@ -2,5 +2,8 @@ import {h} from './link'
 
 export default () =>
 <header>
-  <p class="logo">LOGO</p><h3>Shining Chick</h3>
+  <div class="logo">
+    <img src="favicon.png" />
+  </div>
+  <h3>Shining Chick</h3>
 </header>
